cc41db936ac79136d21bc408096182d92c36fe29
a_guri <aleix.segon@manwin.com>
2012-12-17 11:13:25 +0000

    first commit

---
+++ jenkinsRepo/jenkinsRepoTests/jenkinsRepoTests.h
@@ -0,0 +1,13 @@
+//
+//  jenkinsRepoTests.h
+//  jenkinsRepoTests
+//
+//  Created by  on 12/17/12.
+//  Copyright (c) 2012 com.companyname. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface jenkinsRepoTests : SenTestCase
+
+@end
---
+++ jenkinsRepo/jenkinsRepoTests/jenkinsRepoTests.m
@@ -0,0 +1,32 @@
+//
+//  jenkinsRepoTests.m
+//  jenkinsRepoTests
+//
+//  Created by  on 12/17/12.
+//  Copyright (c) 2012 com.companyname. All rights reserved.
+//
+
+#import "jenkinsRepoTests.h"
+
+@implementation jenkinsRepoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in jenkinsRepoTests");
+}
+
+@end
